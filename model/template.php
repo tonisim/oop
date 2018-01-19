00a6335f96775a55ef509fb5cdf5e1764503f5d2
Tõnis <tonis-ingvar.mals@khk.ee>
2018-01-19 10:30:09 +0200

    Muutujate väärtuste ja tühiväärtuste sisestamine

---
+++ model/template.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tõnis
+ * Date: 19.01.2018
+ * Time: 10:22
+ */
+
+class template
+{
+    //klassi muutujad
+    var $file =  ''; // HTML malli faili nimi
+    var $content = false; // HTML malli sisu, mis alguses  puudub, HTML failist loetud sisu
+    var $vars = array(); // HTML ammli elementide nimetuste ja reaalväärtuste paarid
+
+
+}
