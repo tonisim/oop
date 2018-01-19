87891eb14149fabb241077750ce96bdbc86d87a3
Tõnis <tonis-ingvar.mals@khk.ee>
2018-01-19 09:48:48 +0200

    conf.php faili loomine ning define-ga kaustade määramine

---
+++ conf.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tõnis
+ * Date: 19.01.2018
+ * Time: 9:44
+ */
+define('MODEL_DIR', 'model/');
+define('VIEWS_DIR', 'views/');
+define('CONTROL_DIR', 'controllers/');
